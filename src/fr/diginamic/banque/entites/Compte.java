1c12193d55d09c4a8589ee540a06fb071a47caeb
kevin <kevin.ulreich@epitech.eu>
2021-12-11 11:38:24 +0100

    TP-6 Methode redefinie et affichage

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -5,9 +5,14 @@ public class Compte {
 	public int soldeCompte;
 	
 	public Compte(int numeroCompte, int soldeCompte ) {
-		this.numeroCompte = numeroCompte;
-		this.soldeCompte = soldeCompte;
+		 this.numeroCompte = numeroCompte;
+		 this.soldeCompte = soldeCompte;
 	}
+		 public String toString() {
+			 return "Numero compte : " + this.numeroCompte + "\nSolde compte : " + this.soldeCompte;
+				    
+	}
+	
 	
 	
 }
