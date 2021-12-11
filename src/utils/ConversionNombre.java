b9c77f183f72c6958fa97b555a5556b51f6b6421
kevin <kevin.ulreich@epitech.eu>
2021-12-11 10:28:11 +0100

    TP-4 Methodes static

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String chaine = "12";
+		int valeur = Integer.parseInt(chaine);
+		System.out.println(valeur);
+		int a = 10;
+		int b = 15;
+		System.out.println(Integer.max(a,b));
+	}
+
+}
